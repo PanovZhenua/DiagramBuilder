61a4cac37133fb1f0a7e7e028f367f17086a0868
PanovZhenua <146126039+PanovZhenua@users.noreply.github.com>
2025-11-12 02:58:52 +0500

    Add files via upload

---
+++ DiagramBuilder/Models/ConnectionLine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Windows.Shapes;
+
+namespace DiagramBuilder.Models
+{
+    /// <summary>
+    /// Связь между элементами диаграммы (линии, стрелки)
+    /// </summary>
+    public class ConnectionLine
+    {
+        public List<Line> Lines { get; set; } = new List<Line>();
+        public DiagramBlock FromBlock { get; set; }
+        public DiagramBlock ToBlock { get; set; }
+        public string FromId { get; set; }
+        public string ToId { get; set; }
+
+        public ConnectionLine()
+        {
+            Lines = new List<Line>();
+        }
+    }
+}
