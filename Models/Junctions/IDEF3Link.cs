c51e901b8965a57ec2cc346518b5ba88fb687344
PanovZhenua <146126039+PanovZhenua@users.noreply.github.com>
2026-03-06 16:30:11 +0500

    Delete Models directory

--- Models/Junctions/IDEF3Link.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiagramBuilder.Models
-{
-    public class IDEF3Link
-    {
-        public string From { get; set; }
-        public string To { get; set; }
-        public string Label { get; set; }
-    }
-}
